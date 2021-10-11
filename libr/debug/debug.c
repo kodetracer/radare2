5cc1c315cd91433659b64ae05f5e7e46057041a1
Lars Haukli <lars@flip.re>
2021-10-11 03:24:27 +0200

    Do not override dbg->tid

--- libr/debug/debug.c
+++ libr/debug/debug.c
@@ -447,7 +447,7 @@ R_API bool r_debug_attach(RDebug *dbg, int pid) {
 	if (dbg->h && dbg->h->attach) {
 		ret = dbg->h->attach (dbg, pid);
 		if (ret) {
-			dbg->tid = pid;
+			// dbg->tid = pid;
 			// dbg->pid = pid;
 			// r_debug_select (dbg, pid, ret);
 			r_debug_select (dbg, dbg->pid, dbg->tid);
@@ -653,6 +653,7 @@ R_API bool r_debug_select(RDebug *dbg, int pid, int tid) {
 			return false;
 		}
 	}
+	/*
 	dbg->pid = pid;
 	dbg->tid = tid;
 	if (dbg->tid != -1) {
@@ -664,6 +665,7 @@ R_API bool r_debug_select(RDebug *dbg, int pid, int tid) {
 	} else {
 		R_LOG_ERROR ("Cannot find pid for child %d", dbg->pid);
 	}
+	*/
 
 	// Synchronize with the current thread's data
 	if (dbg->coreb.core) {
