59cd0c74d5188d0e633fd06b838f3f35b5b186d1
Lars Haukli <lars@flip.re>
2023-02-06 19:51:32 +0100

    Disable status in thread json encoding

--- libr/debug/pid.c
+++ libr/debug/pid.c
@@ -112,7 +112,7 @@ R_API bool r_debug_thread_list(RDebug *dbg, int pid, char fmt) {
 				pj_o (j);
 				pj_kb (j, "current", dbg->tid == p->pid);
 				pj_ki (j, "pid", p->pid);
-				pj_ks (j, "status", &p->status);
+				// pj_ks (j, "status", &p->status);
 				pj_ks (j, "path", r_strbuf_get (path));
 				pj_end (j);
 				break;
